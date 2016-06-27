ec7f0b6975cad4a625242b1db075e9101dba21de
Zane Bradley <zanegbradley@gmail.com>
2016-06-27 16:08:30 -0700

    authentication work
    added sdk.js, removed smartcar.js, cleaned up util.js

--- util.js
+++ util.js
@@ -1,27 +1,37 @@
 var rp = require('request-promise')
 var Util = function(){};
 
-API = 'https://api.smartcar.com/v1.0/vehicles'
-
-Util.prototype.sendGetRequest = function(id, endpoint){
-    var url = API
-    if (id) url += '/' + id
-    if (endpoint) url += '/' + endpoint
+getUrl = function(id, endpoint){
+    var url = 'https://api.smartcar.com/v1.0/vehicles'
+    if (id) url += '/' + id;
+    if (endpoint) url += '/' + endpoint;
+    return url;
+}
+request = function(url, method){
     return rp({
-        url: API,
-        method: 'GET',
-        auth: {
-            bearer: token
-        },
+        url: url,
+        method: method,
         json: true
     })
 }
-
-Util.prototype.getJSON = function(endpoint){
-    return function(vehicleID){
-        sendGetRequest(vehicleID, endpoint)
+get = function(endpoint, vid, key){
+    return function(){
+        request(getUrl(vid, endpoint), 'GET')
+        .auth(null, null, true, 'token')
         .then(function(response){
-            return response
-        }).catch(console.log)
+            return (key ? response[key] : response);
+        }).catch(console.log);
+    }
+}
+action = function(endpoint, vid, action, key){
+    return function(arg){
+        data = { action: action };
+        if(key && arg) data[key] = arg;
+        request(getUrl(vid, endpoint), 'POST')
+        .auth(null, null, true, 'token')
+        .form(data)
+        .then(function(response){
+            return response;
+        });
     }
 }
