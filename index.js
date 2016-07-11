d104a60e72c89de5874dbee2869200c36b00e010
Zane Bradley <zanegbradley@gmail.com>
2016-07-11 12:23:29 -0700

    move constants to config.json, handle errors from util.request

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var Vehicle = require('./lib/vehicle');
 var Promise = require('bluebird');
 var util = require('./lib/util');
 
-var AUTH = 'https://auth.smartcar.com/oauth/token';
 /**
  * Initializes Smartcar object
  * @constructor
@@ -64,7 +63,7 @@ Smartcar.prototype.setCreation = function(access) {
  */
 Smartcar.prototype.exchangeCode = function(code) {
   return util.request({
-    uri: AUTH,
+    uri: util.config.auth,
     method: 'POST',
     auth: this.auth,
     form: {
@@ -84,7 +83,7 @@ Smartcar.prototype.exchangeCode = function(code) {
  */
 Smartcar.prototype.exchangeToken = function(refresh_token) {
   return util.request({
-    uri: AUTH,
+    uri: util.config.auth,
     method: 'POST',
     auth: this.auth,
     form: {
