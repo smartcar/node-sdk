311d3b508947d4afb29a8835b33a96cc627d3542
Gurpreet Atwal <git@gatwal.com>
2017-07-05 20:55:24 +0000

    refactor(index): move Client into its own file
    BREAKING CHANGE: two methods moved off of the client prototype and
    client has been renamed to AuthClient
    
    smartcar.Client -> smartcar.AuthClient
    client.getVehicles -> smartcar.getVehicles
    client.expired -> smartcar.expired

--- index.js
+++ index.js
@@ -2,143 +2,26 @@
 
 const Promise = require('bluebird');
 const _ = require('lodash');
-const qs = require('querystring');
 
-const Vehicle = require('./lib/vehicle');
-const config = require('./lib/config');
-const errors = require('./lib/errors');
 const util = require('./lib/util');
 
-/**
- * @type {Object}
- * @typedef Access
- * @property {String} tokenType - Always set to `Bearer`
- * @property {Number} expiresIn - Number of seconds the access token is valid for, always set to 7200 (2 hours)
- * @property {String} expiration - ISO 8601 Datetime string which represents when the token expires
- * @property {String} accessToken - A token to be used for requests to the Smartcar API
- * @property {String} refreshToken - A token which is used to renew access when the current access token expires, expires in 60 days
- *
- * @example
- * {
- *   "tokenType": "example",
- *   "expiresIn": 3600,
- *   "expiration": "2017-05-26T01:21:27.070Z",
- *   "accessToken": "88704225-9f6c-4919-93e7-e0cec71317ce",
- *   "refreshToken": "60a9e801-6d26-4d88-926e-5c7f9fc13486",
- * }
- */
-
-/**
- * Create a Smartcar API client for your application.
- *
- * @constructor
- * @param {Object} options
- * @param {String} options.clientId - The application's client id
- * @param {String} options.clientSecret - The application's client secret
- * @param {String} options.redirectUri - one of the application's preregistered redirect URIs
- * @param {String[]} [options.scope] - list of permissions to request from user
- */
-function Client(options) {
-  this.clientId = options.clientId;
-  this.clientSecret = options.clientSecret;
-  this.redirectUri = options.redirectUri;
-  this.scope = options.scope;
-
-  this.request = util.request.defaults({
-    baseUrl: config.auth,
-    auth: {
-      user: this.clientId,
-      pass: this.clientSecret,
-    },
-    transform: function(body, res, full) {
-      res.body = util.setExpiration(res.body);
-      return full ? res : res.body;
-    },
-  });
-
-}
-
-/**
- * Generate the OAuth authorization URL for a given vehicle make.
- *
- * By default users are not shown the permission dialog if they have already
- * approved the set of scopes for this application. The application can elect
- * to always display the permissions dialog to the user by setting
- * approval_prompt to `force`.
- *
- * @param {String} make - the vehicle make to generate the URL for
- * @param {Object} [options]
- * @param {String} [options.state] - extra application state to pass along
- * @param {Boolean} [options.approval_prompt=auto] - see 2nd paragraph above
- * @return {String} OAuth authorization URL to redirect user to
- */
-Client.prototype.getAuthUrl = function(make, options) {
-
-  /* eslint-disable camelcase */
-  const parameters = {
-    response_type: 'code',
-    client_id: this.clientId,
-    redirect_uri: this.redirectUri,
-  };
-  /* eslint-enable camelcase */
-
-  if (this.scope) {
-    parameters.scope = this.scope.join(' ');
-  }
-
-  _.defaults(parameters, options);
-  const query = qs.stringify(parameters);
-
-  return `${config.oems[make]}/oauth/authorize?${query}`;
+/* eslint-disable global-require */
+const smartcar = {
+  errors: require('./lib/errors'),
+  Vehicle: require('./lib/vehicle'),
+  AuthClient: require('./lib/auth-client'),
 };
-
-
-/**
- * Exchange an authorization code for an access object.
- *
- * @param {String} code - authorization code to exchange
- * @return {Promise<Access>}
- */
-Client.prototype.exchangeCode = function(code) {
-
-  /* eslint-disable camelcase */
-  const form = {
-    code,
-    grant_type: 'authorization_code',
-    redirect_uri: this.redirectUri,
-  };
-  /* eslint-enable camelcase */
-
-  return util.wrap(this.request.post('/oauth/token', {form}));
-};
-
-/**
- * Exchange a refresh token for a new access object.
- *
- * @param {String} token - refresh token to exchange
- * @return {Promise<Access>}
- */
-Client.prototype.exchangeToken = function(token) {
-
-  /* eslint-disable camelcase */
-  const form = {
-    grant_type: 'refresh_token',
-    refresh_token: token,
-  };
-  /* eslint-enable camelcase */
-
-  return util.wrap(this.request.post('/oauth/token', {form}));
-
-};
+/* eslint-enable global-require */
 
 /**
  * Check if an access object's access token is expired.
  *
- * @param {Access} access - access object to be checked
+ * @param {Access|String} access - access object or expiration to be checked
  * @return {Boolean} true if expired, false if not expired
  */
-Client.prototype.expired = function(access) {
-  const expiration = Date.parse(access.expiration);
+smartcar.expired = function(access) {
+  let expiration = _.isString(access) ? access : access.expiration;
+  expiration = Date.parse(expiration);
 
   if (!Number.isFinite(expiration)) { // eslint-disable-next-line max-len
     throw new TypeError('"access.expiration" argument must be a valid ISO date string');
@@ -156,7 +39,7 @@ Client.prototype.expired = function(access) {
  * @param {Number} [paging.offset] - index to start vehicle list
  * @return {Promise}
  */
-Client.prototype.getVehicles = Promise.method(function(token, paging) {
+smartcar.getVehicles = Promise.method(function(token, paging) {
 
   if (typeof token !== 'string') {
     throw new TypeError('"token" argument must be a string');
@@ -170,11 +53,5 @@ Client.prototype.getVehicles = Promise.method(function(token, paging) {
   });
 
 });
-
-const smartcar = {};
-
-smartcar.errors = errors;
-smartcar.Vehicle = Vehicle;
-smartcar.Client = Client;
 
 module.exports = smartcar;
