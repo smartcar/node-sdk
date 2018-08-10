29f5617d7e5dee0079dd528fabce470d347f2378
Karthik Bhaskara <karthikbhaskara96@gmail.com>
2018-08-10 14:32:48 -0700

    cr

--- test/end-to-end/helpers/index.js
+++ test/end-to-end/helpers/index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const Chance = require('chance');
+const querystring = require('querystring');
 
 const chance = new Chance();
+const redirectUri = 'http://localhost:4040/callback';
 
 const helpers = {};
 
@@ -10,30 +12,25 @@ helpers.getAuthClientParams = function() {
   return {
     clientId: 'e922556a-7d4f-4168-88cd-059276044798',
     clientSecret: '79c07401-d3b2-48c0-8407-dc19c4ece7ff',
-    redirectUri: 'http://localhost:4040/callback',
+    redirectUri,
     development: true,
   };
 };
 
-const getCodeFromUrl = function(uri) {
+const getCodeFromUri = function(uri) {
   // this helper functions assumes the structure of the uri
   // is valid and contains the code required
   const firstIndexOfParams = uri.indexOf('?') + 1;
   const params = uri.substring(firstIndexOfParams);
 
-  for (const param of params.split('&')) {
-    const [code, value] = param.split('=');
-    if (code === 'code') {
-      return value;
-    }
-  }
+  return querystring.parse(params).code;
 };
 
 helpers.runTest = function(client, browser, authUrl, test, done) {
   const email = chance.email({domain: 'smartcar.com'});
   /*
   * (todo Karthik) remove the pauses
-  * we will use pause here to allow for the callback to execute
+  * we will use pause here to give time for the callback to execute
   * until we can neatly run an async nightwatch callback
   **/
   browser
@@ -47,9 +44,9 @@ helpers.runTest = function(client, browser, authUrl, test, done) {
     .setValue('input[id=password]', 'password')
     .click('button[id=approval-button]')
     .url((currentUrl) => {
-      if (currentUrl.value.startsWith('http://localhost:4040/callback')) {
+      if (currentUrl.value.startsWith(redirectUri)) {
         // if we have skipped the permissions, we can extract the code
-        test(getCodeFromUrl(currentUrl.value));
+        test(getCodeFromUri(currentUrl.value));
         browser
           .pause(5000)
           .end();
@@ -60,7 +57,7 @@ helpers.runTest = function(client, browser, authUrl, test, done) {
           .waitForElementVisible('div[class=permissions]', 5000)
           .click('button[id=approval-button]')
           .url((currentUrl) => {
-            test(getCodeFromUrl(currentUrl.value));
+            test(getCodeFromUri(currentUrl.value));
           })
           .pause(5000)
           .end();
