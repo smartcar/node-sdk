70fb147d5f19d1a7c458128240338aa15494c052
Karthik Bhaskara <karthikbhaskara96@gmail.com>
2019-06-25 15:40:09 -0700

    fix: build failures

--- test/end-to-end/helpers/index.js
+++ test/end-to-end/helpers/index.js
@@ -32,11 +32,9 @@ helpers.getAuthClientParams = () => ({
     'read_odometer',
     'control_security',
     'read_vin',
-    /*
-     *'read_fuel',
-     *'read_battery',
-     *'read_charge',
-     */
+    'read_fuel',
+    'read_battery',
+    'read_charge',
   ],
   testMode: true,
 });
