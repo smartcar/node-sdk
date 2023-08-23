b50f2f389eb6a615d9406fa96265207ccda5c146
Adrian Garcia <joadgaji>
2023-08-23 10:09:19 -0700

    Revert read_security

--- test/end-to-end/helpers/index.js
+++ test/end-to-end/helpers/index.js
@@ -27,7 +27,6 @@ helpers.DEFAULT_SCOPES = [
   'required:read_location',
   'required:read_odometer',
   'required:control_security',
-  'required:read_security',
   'required:read_vin',
   'required:read_fuel',
   'required:read_battery',
